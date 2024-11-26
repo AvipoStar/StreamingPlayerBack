c0032ee3b508437683f2565bae22593c82a182d8
FedorovKD <FedorovKD>
2024-11-26 23:12:25 +0700

    delete files

--- admin-service/models/login.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class LoginClass(BaseModel):
-    email: str
-    password: str
--- admin-service/models/token.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class Token(BaseModel):
-    value: str
